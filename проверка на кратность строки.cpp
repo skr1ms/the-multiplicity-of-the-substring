56c81f487bedcb4af385b29bcc439964ce9a8f29
Takuya Yagami <145888123+skr1ms@users.noreply.github.com>
2024-09-01 17:31:34 +0300

    Add files via upload

---
+++ проверка на кратность строки.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool IsKPeriodic(string s, int k) {
+	//проверка длины строки
+	if (s.length() <= NULL) 
+		return false;
+	if (s.length() % k != 0) {
+		return false;  
+	}
+	else {
+		std::string substring = s.substr(0, k); //если находится подстрока, то сохраняем ее в substring
+		for (int i = k; i < s.length(); i += k) {
+			//проверяем на равенство подстрок
+			if (s.substr(i, k) != substring) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
+
+int main()
+{
+	string q{ "abcabcabcabc" };
+
+	bool a = IsKPeriodic(q, 5);
+
+	cout << a << endl;
+	return 0;
+}
